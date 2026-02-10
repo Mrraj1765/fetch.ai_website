48da1ff51dd437a77a8b4e3f06c9084df68b5c85
Tejaspatil1175 <tejaspatil1175@gmail.com>
2026-02-10 22:58:16 +0530

    created navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,34 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
 
+
+// jste router use nahi kele ahe ajun 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="app">
+      <Header />
+      <main className="main-content">
+       {/* new pages routes will add here so ithe taka */}
+      </main>
+    </div>
   )
 }
-
+// this componet export succesfull ok 
 export default App
